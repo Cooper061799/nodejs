9416812ebed5e6b7686f3143e2363a5c97176421
Cooper Godfrey <Cooper.Godfrey@bloomenergy.com>
2023-03-11 21:21:51 -0500

    Variables

---
+++ 01050 variables.js
@@ -0,0 +1,4 @@
+message = 'Hello World';
+console.log(message);
+days = 7;
+console.log(days)
---
+++ 01060 declaringvariables.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Declare the variable message and assign the value 'Hello' to it
+let message = 'Hello';
+
+console.log(message);
+
+// Assigning new value to already existing variable. It does not need to be "let" or declared again
+message = 'How are you?';
+
+console.log(message);
